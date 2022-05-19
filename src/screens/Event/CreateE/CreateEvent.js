40c3b3686a9226381e67edac581acfda2381da73
DanSousa <dansousac2@gmail.com>
2022-05-19 17:52:53 -0300

    begin with the consume API

--- src/screens/Event/CreateE/CreateEvent.js
+++ src/screens/Event/CreateE/CreateEvent.js
@@ -2,19 +2,31 @@ import React from "react";
 import './CreateEvent.css'
 import 'bootswatch/dist/vapor/bootstrap.css';
 import FormGroup from "../../../componentes/FormGroup";
+import axios from "axios";
 
 export default class CreateEvent extends React.Component {
     state = {
         eventName:"",
         date:"",
-        address:""
+        address:"",
+        budget:""
     }
     
     
     
     post = () => {
-        console.log("Event name = " + this.state.eventName + " Event Date: " + this.state.date + " Event Address: " + this.state.address);
-        alert("Event created!")
+        axios.post( 'http://localhost:8080/api/event/save',
+            {
+                eventName: this.state.eventName,
+                date: this.state.date,
+                adress: this.state.address,
+                budget: this.state.budget
+            }
+        ).then( Response => {
+            console.log(Response)
+        }).catch( error => {
+            console.log(error.Response)
+        })
     }
 
     render() {
@@ -34,6 +46,10 @@ export default class CreateEvent extends React.Component {
                         <FormGroup label='Event Address' htmlFor='lab03'>
                             <input className="form-control form-control-lg" type="text" placeholder="address" id="lab03"
                             onChange={(e) => {this.setState({address: e.target.value})}}/>
+                        </FormGroup>
+                        <FormGroup label='Event Budget' htmlFor='lab04'>
+                            <input className="form-control form-control-lg" type="text" placeholder="address" id="lab04"
+                            onChange={(e) => {this.setState({budget: e.target.value})}}/>
                         </FormGroup>
                         <br/>
                         <button type="button" className="btn btn-primary btn-lg" onClick={this.post} >Create</button>
